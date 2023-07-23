fadc8623487df5ae2856b005b1b4c62212fb60d7
Ivaylo Ganev <ivayloganev@gmail.com>
2023-07-23 20:15:23 +0300

    Generate listing details component

--- craftsmen-services-app/src/app/feature/feature.module.ts
+++ craftsmen-services-app/src/app/feature/feature.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { ListingsComponent } from './listings/listings.component';
 import { FeatureRoutingModule } from './feature-routing.module';
 import { CreateListingComponent } from './create-listing/create-listing.component';
+import { ListingDetailsComponent } from './listing-details/listing-details.component';
 
 
 
 @NgModule({
   declarations: [
     ListingsComponent,
-    CreateListingComponent
+    CreateListingComponent,
+    ListingDetailsComponent
   ],
   imports: [
     CommonModule,
