e355760324497422d09a02e99fc6f69476f07378
Ivaylo Ganev <ivayloganev@gmail.com>
2023-07-23 20:13:26 +0300

    Implement routing to feature components

--- craftsmen-services-app/src/app/feature/feature-routing.module.ts
+++ craftsmen-services-app/src/app/feature/feature-routing.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListingsComponent } from './listings/listings.component';
+import { CreateListingComponent } from './create-listing/create-listing.component';
 
 const routes: Routes = [
-  {path: 'listings', component: ListingsComponent},
+  {
+    path: 'listings',
+    children: [
+        {path: '', pathMatch: 'full', component: ListingsComponent},
+        {path: 'create', component: CreateListingComponent}
+    ]
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class FeatureRoutingModule { }
+export class FeatureRoutingModule {}
--- craftsmen-services-app/src/app/feature/feature.module.ts
+++ craftsmen-services-app/src/app/feature/feature.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListingsComponent } from './listings/listings.component';
 import { FeatureRoutingModule } from './feature-routing.module';
+import { CreateListingComponent } from './create-listing/create-listing.component';
 
 
 
 @NgModule({
   declarations: [
-    ListingsComponent
+    ListingsComponent,
+    CreateListingComponent
   ],
   imports: [
     CommonModule,
