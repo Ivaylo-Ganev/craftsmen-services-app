7cbcfa1121d0b55249e93e16eacd2492152e80c1
Ivaylo Ganev <ivayloganev@gmail.com>
2023-07-23 20:20:56 +0300

    Add routing for details component

--- craftsmen-services-app/src/app/feature/feature-routing.module.ts
+++ craftsmen-services-app/src/app/feature/feature-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListingsComponent } from './listings/listings.component';
 import { CreateListingComponent } from './create-listing/create-listing.component';
+import { ListingDetailsComponent } from './listing-details/listing-details.component';
 
 const routes: Routes = [
   {
     path: 'listings',
     children: [
         {path: '', pathMatch: 'full', component: ListingsComponent},
-        {path: 'create', component: CreateListingComponent}
+        {path: 'create', component: CreateListingComponent},
+        {path: ':listingId', component: ListingDetailsComponent}
     ]
   },
 ];
