dfa4e0922b7afa5eb9cf248b130411d5a2699a91
Ivaylo Ganev <ivayloganev@gmail.com>
2023-07-23 19:28:49 +0300

    Generate modules and components
    Generate login, register and profile components in user module. Generate home component inside the core module. Generate feature module and a listings component inside it.

--- craftsmen-services-app/src/app/core/core.module.ts
+++ craftsmen-services-app/src/app/core/core.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule
---
+++ craftsmen-services-app/src/app/feature/feature.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListingsComponent } from './listings/listings.component';
+
+
+
+@NgModule({
+  declarations: [
+    ListingsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FeatureModule { }
