73a9cba840add5a4ea7aaee07fd4106008ab10c4
Ivaylo Ganev <ivayloganev@gmail.com>
2023-08-04 22:54:57 +0300

    Update profile component template and routing

--- craftsmen-services-app/src/app/user/user-routing.module.ts
+++ craftsmen-services-app/src/app/user/user-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ListingDetailsComponent } from '../feature/listing-details/listing-details.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'profile', component: ProfileComponent},
+  {path: 'profile/:listingId', component: ListingDetailsComponent},
 ];
 
 @NgModule({
